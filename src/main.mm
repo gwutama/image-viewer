d34ed57d0a9dacc3415f6dc9d4ad687c86f72d69
Galuh Utama <galuh.utama@gwutama.de>
2024-09-20 01:07:53 +0200

    Dark mode, baby!

--- src/main.mm
+++ src/main.mm
@@ -2,10 +2,16 @@
 #include "ImageEditor.h"
 #include <opencv2/opencv.hpp>
 
+// This will include the necessary Objective-C headers
+#ifdef __WXMAC__
+#import <AppKit/AppKit.h>
+#endif
+
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
+    void ForceDarkMode();
 };
 
 class MyFrame : public wxFrame
@@ -18,17 +24,32 @@ private:
 
     void OnOpen(wxCommandEvent& event);
     void CreateMenuBar();
-    void UpdateZoomStatus(float zoomLevel);
 };
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
+    // Force dark mode on macOS
+    ForceDarkMode();
+
     MyFrame* frame = new MyFrame("Image Viewer");
     frame->Show(true);
     frame->Maximize(true);  // Maximize the window on start
     return true;
+}
+
+void MyApp::ForceDarkMode()
+{
+#ifdef __WXMAC__
+    // Use Objective-C API to force dark mode on macOS
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"NSRequiresAquaSystemAppearance"];
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"AppleInterfaceStyleSwitchesAutomatically"];
+    [[NSUserDefaults standardUserDefaults] setObject:@"Dark" forKey:@"AppleInterfaceStyle"];
+
+    // Force the appearance update
+    NSApp.appearance = [NSAppearance appearanceNamed:NSAppearanceNameDarkAqua];
+#endif
 }
 
 MyFrame::MyFrame(const wxString& title)
@@ -40,8 +61,9 @@ MyFrame::MyFrame(const wxString& title)
     // Create the menu bar
     CreateMenuBar();
 
-    // Create a status bar
-    CreateStatusBar();
+    // Set dark background and foreground colors
+    SetBackgroundColour(wxColour(40, 40, 40));
+    SetForegroundColour(wxColour(255, 255, 255));
 }
 
 void MyFrame::OnOpen(wxCommandEvent& event)
